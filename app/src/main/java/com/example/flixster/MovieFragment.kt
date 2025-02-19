5af26bce1232b107561b9218c5c6c900e74607cb
MrJigglys <djtheman007@gmail.com>
2025-02-18 21:17:12 -0500

    Fixed GridLayoutManager. Added demo.gif

--- app/src/main/java/com/example/flixster/MovieFragment.kt
+++ app/src/main/java/com/example/flixster/MovieFragment.kt
@@ -31,7 +31,7 @@ class MovieFragment : Fragment(), OnListFragmentInteractionListener{
         val progressBar = view.findViewById<View>(R.id.progress) as ContentLoadingProgressBar
         val recyclerView = view.findViewById<View>(R.id.list) as RecyclerView
         val context = view.context
-        recyclerView.layoutManager = GridLayoutManager(context, 2)
+        recyclerView.layoutManager = GridLayoutManager(context, 1)
         updateAdapter(progressBar, recyclerView)
         return view
     }
